a6276658d2528372781cb20dd6ccabbcf9102883
followfork <xuwei_james@163.com>
2016-06-20 23:40:24 +0800

    add webpack store

---
+++ vuex/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+	notes: [],
+	activeNote: {}
+}
+
+const mutations = {
+	ADD_NOTE(state) {
+		const newNote = {
+			text: 'New',
+			favorite: false
+		}
+		state.notes.push(newNote)
+		state.activeNote = newNote
+	},
+
+	EDIT_NOTE(state, text) {
+		state.activeNote.text = text
+	},
+
+	DELETE_NOTE(state) {
+		state.notes.$remove(state.activeNote)
+		state.activeNote = state.notes[0]
+	},
+
+	TOGGLE_FAVORITE(state, note) {
+		state.activeNote = note
+	}
+}
+
+export default new Vuex.Store({
+	state,
+	mutations
+})
